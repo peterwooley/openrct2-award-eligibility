172abe6f5ef69cbff16d0e02307a645bb66bc68d
Peter Wooley <peterwooley@gmail.com>
2025-04-24 17:25:01 -0700

    Added default keybinding to open window (Shift+A).

--- src/startup.ts
+++ src/startup.ts
@@ -5,5 +5,14 @@ export function startup() {
     ui.registerMenuItem("Award Eligibility", () => {
       getAwardsWindow().open();
     });
+
+    ui.registerShortcut({
+			id: "oae.window",
+			text: "Show award eligibility",
+			bindings: ["Shift+A"],
+			callback: function() {
+				getAwardsWindow().open();
+			},
+		});
   }
 }
